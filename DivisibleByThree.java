91878a83131cef4e42deca65d585c5f23230aeeb
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-07-22 14:25:56 -0400

    Create DivisibleByThree.java

---
+++ DivisibleByThree.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class DivisibleByThree {
+
+    public static void main(String[] args) {
+    }
+    
+    public static void divisibleByThreeInRange(int first, int second) {
+        for (int i = first; i <= second; i++) {
+            if(i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
