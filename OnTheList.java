ccea467aa872fbeead30535333fdaccb3349e404
Isaac Rendon <38363661+irendon1993@users.noreply.github.com>
2022-08-09 10:06:58 -0400

    Create OnTheList.java

---
+++ OnTheList.java
@@ -0,0 +1,23 @@
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            list.add(input);
+        }
+        
+        System.out.println("Search for?");
+        String searchName = scanner.nextLine();
+        
+        if(list.contains(searchName)){
+            System.out.println(searchName + " was found!");
+        } else {
+            System.out.println(searchName + " was not found!");
+        }   
+    }
+}
