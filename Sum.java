c96d4d17a1cf443169303462d606cfa4b376d9cf
Isaac Rendon <38363661+irendon1993@users.noreply.github.com>
2022-08-11 13:09:36 -0400

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Sum {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        System.out.println(sum(numbers));
+
+        numbers.add(5);
+        numbers.add(1);
+        System.out.println(sum(numbers));
+    }
+    
+    public static int sum(ArrayList<Integer> numbers){
+        //return the sum of the numbers in the parameter list.
+        int sum = 0;
+        for(int number:numbers) {
+           sum += number;
+        }
+        return sum;
+    }
+}
