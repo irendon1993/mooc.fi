020f1272ccab5c46fb264b6982004cf92e53bad4
Isaac Rendon <38363661+irendon1993@users.noreply.github.com>
2022-08-17 06:01:29 -0400

    Create Main.java

---
+++ TelevisionProgram/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // implement here your program that uses the TelevisionProgram class
+
+        ArrayList<TelevisionProgram> programs = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        
+        while(true){
+            System.out.println("Name: ");
+            String nameInput = scanner.nextLine();
+            
+            if(nameInput.isEmpty()){
+                break;
+            }
+            
+            System.out.println("Duration:");
+            int durationInput = Integer.valueOf(scanner.nextLine());
+            
+            programs.add(new TelevisionProgram(nameInput, durationInput));
+        }
+        System.out.println("Program's maximum duration?");
+        int maximumDuration = Integer.valueOf(scanner.nextLine());
+        
+        for(TelevisionProgram program: programs){
+            if(program.getDuration() <= maximumDuration){
+                System.out.println(program);
+            }
+        }
+    }
+}
